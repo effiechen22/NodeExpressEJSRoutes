e3c5784f82245ce76b82e81ed9e6101659d31db8
effiechen22 <effiechen22@gmail.com>
2023-08-26 20:05:42 +0800

    fix: body-parser 執行順序需要移動到 視圖引擎之前才能順利解析資料 feat: 載入並應用路由 searchRouter

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ app.engine('ejs', engine); // 使用 ejs-locals 作為 EJS 引擎
 app.set('views', './views'); // 設定視圖的存放目錄為 views 目錄
 app.set('view engine', 'ejs'); // 設定視圖引擎為 EJS
 
+// 載入 body-parser 
+const bodyParser = require('body-parser');
+// 使用 body-parser : 用途為body解析
+app.use(bodyParser.urlencoded({extended:false})); //處理 x-www-form-urlencoded 格式的POST資料
+app.use(bodyParser.json()) //bodyParser.json() 處理 json 格式的POST資料
 
 
 // 載入路由
 const homeRouter = require('./routes');
 const userRouter = require('./routes/user');
+const searchRouter = require('./routes/search');
 
 // 將路由模組應用到路徑上
 app.use('/', homeRouter);
 app.use('/user', userRouter);
+app.use('/search', searchRouter);
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({extended:false}));
-// app.use(bodyParser.json())
 
 
 
